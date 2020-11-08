d4bbe503dd1fb8d68d3d1c6e249d9a7846152144
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-11-08 08:07:59 +0700

    Initial commit

---
+++ lib/App/GrepUtils.pm
@@ -0,0 +1,16 @@
+package App::GrepUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to the Unix command 'grep'
+
+=head1 DESCRIPTION
+
+This distribution includes the following CLI utilities related to the Unix
+command C<grep>:
+
+# INSERT_EXECS_LIST
+
+=cut
